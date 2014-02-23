6ef97315b20bf8e7e876d2f56aeee36f8f0bd169
destrostudios@gmail.com <destrostudios@gmail.com@926487a3-12ae-5666-ef0a-07251ff29e85>
2014-02-23 00:36:56 +0000

    The camera can now be zoomed in/out with the mouse wheel.

--- trunk/Amara/src/amara/engine/applications/ingame/client/appstates/IngameCameraAppState.java
+++ trunk/Amara/src/amara/engine/applications/ingame/client/appstates/IngameCameraAppState.java
@@ -7,8 +7,10 @@ package amara.engine.applications.ingame.client.appstates;
 import com.jme3.app.Application;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.math.Vector3f;
 
 /**
@@ -22,6 +24,7 @@ public class IngameCameraAppState extends ClientBaseAppState implements ActionLi
     }
     private boolean[] moveDirections = new boolean[4];
     private float movementSpeed = 20;
+    private float zoomFactor = 2;
     
     @Override
     public void initialize(AppStateManager stateManager, Application application){
@@ -31,8 +34,10 @@ public class IngameCameraAppState extends ClientBaseAppState implements ActionLi
         mainApplication.getInputManager().addMapping("camera_right", new KeyTrigger(KeyInput.KEY_RIGHT));
         mainApplication.getInputManager().addMapping("camera_down", new KeyTrigger(KeyInput.KEY_DOWN));
         mainApplication.getInputManager().addMapping("camera_left", new KeyTrigger(KeyInput.KEY_LEFT));
+        mainApplication.getInputManager().addMapping("camera_zoom_in", new MouseAxisTrigger(MouseInput.AXIS_WHEEL, false));
+        mainApplication.getInputManager().addMapping("camera_zoom_out", new MouseAxisTrigger(MouseInput.AXIS_WHEEL, true));
         mainApplication.getInputManager().addListener(this, new String[]{
-            "camera_up","camera_right","camera_down","camera_left"
+            "camera_up","camera_right","camera_down","camera_left","camera_zoom_in","camera_zoom_out"
         });
     }
 
@@ -77,5 +82,23 @@ public class IngameCameraAppState extends ClientBaseAppState implements ActionLi
         else if(actionName.equals("camera_left")){
             moveDirections[3] = value;
         }
+        else if(actionName.equals("camera_zoom_out")){
+            zoom(false);
+        }
+        else if(actionName.equals("camera_zoom_in")){
+            zoom(true);
+        }
+    }
+    
+    private void zoom(boolean inOrOut){
+        Vector3f zoomDirection = getZoomDirection();
+        if(!inOrOut){
+            zoomDirection.negateLocal();
+        }
+        mainApplication.getCamera().getLocation().addLocal(zoomDirection);
+    }
+    
+    private Vector3f getZoomDirection(){
+        return mainApplication.getCamera().getDirection().mult(zoomFactor);
     }
 }
