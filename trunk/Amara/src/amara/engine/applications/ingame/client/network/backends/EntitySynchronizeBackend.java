d66954dfaf05553418d76dde636cd128b4a10b5f
destrostudios@gmail.com <destrostudios@gmail.com@926487a3-12ae-5666-ef0a-07251ff29e85>
2014-04-05 18:14:42 +0000

    The entity world synchronizing is now enqueued to the main thread's update loop (Fixed some rare bugs, where observers updated their lists in between the synchronizing leading to wrong results).

--- trunk/Amara/src/amara/engine/applications/ingame/client/network/backends/EntitySynchronizeBackend.java
+++ trunk/Amara/src/amara/engine/applications/ingame/client/network/backends/EntitySynchronizeBackend.java
@@ -5,6 +5,7 @@
 package amara.engine.applications.ingame.client.network.backends;
 
 import com.jme3.network.Message;
+import amara.engine.applications.DisplayApplication;
 import amara.engine.network.*;
 import amara.engine.network.messages.entitysystem.Message_EntityChanges;
 import amara.game.entitysystem.EntityWorld;
@@ -16,36 +17,44 @@ import amara.game.entitysystem.synchronizing.*;
  */
 public class EntitySynchronizeBackend implements MessageBackend{
 
-    public EntitySynchronizeBackend(EntityWorld entityWorld){
+    public EntitySynchronizeBackend(DisplayApplication displayApplication, EntityWorld entityWorld){
+        this.displayApplication = displayApplication;
         this.entityWorld = entityWorld;
     }
+    private DisplayApplication displayApplication;
     private EntityWorld entityWorld;
     
     @Override
     public void onMessageReceived(Message receivedMessage, MessageResponse messageResponse){
         if(receivedMessage instanceof Message_EntityChanges){
             Message_EntityChanges message = (Message_EntityChanges) receivedMessage;
-            EntityChange[] entityChanges = message.getEntityChanges();
-            for(int i=0;i<entityChanges.length;i++){
-                EntityChange entityChange = entityChanges[i];
-                if(entityChange instanceof RemovedEntityChange){
-                    RemovedEntityChange removedEntityChange = (RemovedEntityChange) entityChange;
-                    entityWorld.removeEntity(removedEntityChange.getEntityID());
-                }
-                else if(entityChange instanceof NewComponentChange){
-                    NewComponentChange newComponentChange = (NewComponentChange) entityChange;
-                    entityWorld.setComponent(newComponentChange.getEntityID(), newComponentChange.getComponent());
-                }
-                else if(entityChange instanceof RemovedComponentChange){
-                    RemovedComponentChange removedComponentChange = (RemovedComponentChange) entityChange;
-                    try{
-                        Class componentClass = Class.forName(removedComponentChange.getComponentClassName());
-                        entityWorld.removeComponent(removedComponentChange.getEntityID(), componentClass);
-                    }catch(ClassNotFoundException ex){
-                        ex.printStackTrace();
+            final EntityChange[] entityChanges = message.getEntityChanges();
+            displayApplication.enqueueTask(new Runnable(){
+
+                @Override
+                public void run(){
+                    for(int i=0;i<entityChanges.length;i++){
+                        EntityChange entityChange = entityChanges[i];
+                        if(entityChange instanceof RemovedEntityChange){
+                            RemovedEntityChange removedEntityChange = (RemovedEntityChange) entityChange;
+                            entityWorld.removeEntity(removedEntityChange.getEntityID());
+                        }
+                        else if(entityChange instanceof NewComponentChange){
+                            NewComponentChange newComponentChange = (NewComponentChange) entityChange;
+                            entityWorld.setComponent(newComponentChange.getEntityID(), newComponentChange.getComponent());
+                        }
+                        else if(entityChange instanceof RemovedComponentChange){
+                            RemovedComponentChange removedComponentChange = (RemovedComponentChange) entityChange;
+                            try{
+                                Class componentClass = Class.forName(removedComponentChange.getComponentClassName());
+                                entityWorld.removeComponent(removedComponentChange.getEntityID(), componentClass);
+                            }catch(ClassNotFoundException ex){
+                                ex.printStackTrace();
+                            }
+                        }
                     }
                 }
-            }
+            });
         }
     }
 }
