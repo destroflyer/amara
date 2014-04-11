b801c9c2f7c5941eba313f6493ef79f9e06f1637
destrostudios <destrostudios@gmail.com>
2014-04-11 00:56:29 +0000

    Reworked spell cast system to support adding effects to autoattacks/spells, added "empower" spell to the soldier. A unit can no longer hold more than one time the exact same buff entity, instead the remaining duration is refreshed. Fixed a bug, where ingame gui elements (e.g. healthbars) were drawn in front of the actual gui (e.g. the "Game Over" button).

--- Amara/src/amara/game/entitysystem/components/spells/placeholders/TargetedMovementTargetComponent.java
+++ Amara/src/amara/game/entitysystem/components/spells/placeholders/TargetedMovementTargetComponent.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package amara.game.entitysystem.components.spells.targets;
+package amara.game.entitysystem.components.spells.placeholders;
 
 import com.jme3.network.serializing.Serializable;
 
