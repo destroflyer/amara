9e9efe13c0fa58401e4c995ca4baa9670d97a907
destroflyer <destro-flyer@web.de>
2015-10-07 01:40:17 +0200

    Fixed a bug, where instanttriggers were still applied after they were removed, resulting in redundant entities. Adapted cast durations to ganfaul spells.

--- Amara/src/amara/game/entitysystem/systems/effects/triggers/TriggerInstantEffectSystem.java
+++ Amara/src/amara/game/entitysystem/systems/effects/triggers/TriggerInstantEffectSystem.java
@@ -5,6 +5,7 @@
 package amara.game.entitysystem.systems.effects.triggers;
 
 import amara.game.entitysystem.*;
+import amara.game.entitysystem.components.units.effecttriggers.*;
 import amara.game.entitysystem.components.units.effecttriggers.triggers.*;
 
 /**
@@ -15,7 +16,7 @@ public class TriggerInstantEffectSystem implements EntitySystem{
     
     @Override
     public void update(EntityWorld entityWorld, float deltaSeconds){
-        for(int effectTriggerEntity : entityWorld.getEntitiesWithAll(InstantTriggerComponent.class)){
+        for(int effectTriggerEntity : entityWorld.getEntitiesWithAll(InstantTriggerComponent.class, TriggerSourceComponent.class)){
             EffectTriggerUtil.triggerEffect(entityWorld, effectTriggerEntity, -1);
         }
     }
