b2935d43a2190533316a93272c251090215cdde1
destroflyer <destro-flyer@web.de>
2016-01-08 00:59:07 +0100

    [Project 8/16] Seperated the project into smaller parts, allowing further changes to run an extern masterserver more smoothly.

---
+++ Projects/Amara-Library-HeadlessApplication/src/amara/engine/appstates/NetworkClientHeadlessAppState.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amara.engine.appstates;
+
+import amara.engine.network.NetworkClient;
+import amara.engine.network.exceptions.*;
+
+/**
+ *
+ * @author Carl
+ */
+public class NetworkClientHeadlessAppState extends BaseHeadlessAppState{
+
+    public NetworkClientHeadlessAppState(String host, int port) throws ServerConnectionException, ServerConnectionTimeoutException{
+        networkClient.connectToServer(host, port);
+    }
+    private NetworkClient networkClient = new NetworkClient();
+
+    public NetworkClient getNetworkClient(){
+        return networkClient;
+    }
+}
---
+++ Projects/Amara-Library-HeadlessApplication/src/amara/engine/appstates/NetworkServerAppState.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amara.engine.appstates;
+
+import amara.engine.network.NetworkServer;
+import amara.engine.network.exceptions.*;
+
+/**
+ *
+ * @author Carl
+ */
+public class NetworkServerAppState extends BaseHeadlessAppState{
+
+    public NetworkServerAppState(int port) throws ServerCreationException{
+        networkServer.createServer(port);
+        System.out.println("Server started.");
+    }
+    private NetworkServer networkServer = new NetworkServer();
+
+    public NetworkServer getNetworkServer(){
+        return networkServer;
+    }
+}
