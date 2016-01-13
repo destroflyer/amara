e4787748247877da7a6d95e8ba7239d224818f31
destroflyer <destro-flyer@web.de>
2016-01-13 07:45:16 +0100

    Added an utility class that can be used to prepare a clean buld by deleting the projects dist-directories (preventing a SDK error).

---
+++ Projects/Amara-Test/src/amara/launcher/CleanupBuild.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amara.launcher;
+
+import java.io.File;
+import amara.engine.files.FileManager;
+
+/**
+ *
+ * @author Carl
+ */
+public class CleanupBuild{
+    
+    public static void main(String[] args){
+        File projectsDirectory = new File("../../");
+        for(File projectDirectory : projectsDirectory.listFiles()){
+            if((!projectDirectory.getName().equals("Amara-Master-Server-Application"))
+            || (!projectDirectory.getName().equals("Amara-Master-Client-Application"))){
+                File distDirectory = new File(projectDirectory.getPath() + "/dist");
+                if(distDirectory.exists()){
+                    if(!FileManager.deleteFile(distDirectory)){
+                        System.err.println("Couldn't delete: " + distDirectory);
+                    }
+                }
+            }
+        }
+    }
+}
