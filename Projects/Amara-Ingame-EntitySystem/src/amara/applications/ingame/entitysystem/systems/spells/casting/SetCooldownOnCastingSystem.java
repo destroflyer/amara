a57c0075311d8c9bb15f141b1d1768c1366bb362
destroflyer <destro-flyer@web.de>
2016-02-12 19:01:40 +0100

    Actives can now be unique (adapted the item "zhonyas hourglass").

--- Projects/Amara-Ingame-EntitySystem/src/amara/applications/ingame/entitysystem/systems/spells/casting/SetCooldownOnCastingSystem.java
+++ Projects/Amara-Ingame-EntitySystem/src/amara/applications/ingame/entitysystem/systems/spells/casting/SetCooldownOnCastingSystem.java
@@ -4,7 +4,9 @@
  */
 package amara.applications.ingame.entitysystem.systems.spells.casting;
 
+import amara.applications.ingame.entitysystem.components.general.*;
 import amara.applications.ingame.entitysystem.components.input.*;
+import amara.applications.ingame.entitysystem.components.items.*;
 import amara.applications.ingame.entitysystem.components.spells.*;
 import amara.libraries.entitysystem.*;
 
@@ -18,11 +20,36 @@ public class SetCooldownOnCastingSystem implements EntitySystem{
     public void update(EntityWorld entityWorld, float deltaSeconds){
         for(int casterEntity : entityWorld.getEntitiesWithAll(CastSpellComponent.class)){
             int spellEntity = entityWorld.getComponent(casterEntity, CastSpellComponent.class).getSpellEntity();
-            setOnCooldown(entityWorld, spellEntity);
+            setOnCooldown(entityWorld, casterEntity, spellEntity);
         }
     }
     
-    public static void setOnCooldown(EntityWorld entityWorld, int spellEntity){
+    public static void setOnCooldown(EntityWorld entityWorld, int casterEntity, int spellEntity){
+        setOnCooldown(entityWorld, spellEntity);
+        //Unique Actives
+        if(entityWorld.hasComponent(spellEntity, UniqueComponent.class)){
+            NameComponent nameComponent = entityWorld.getComponent(spellEntity, NameComponent.class);
+            if(nameComponent != null){
+                InventoryComponent inventoryComponent = entityWorld.getComponent(casterEntity, InventoryComponent.class);
+                if(inventoryComponent != null){
+                    for(int itemEntity : inventoryComponent.getItemEntities()){
+                        ItemActiveComponent itemActiveComponent = entityWorld.getComponent(itemEntity, ItemActiveComponent.class);
+                        if(itemActiveComponent != null){
+                            int tmpSpellEntity = itemActiveComponent.getSpellEntity();
+                            if((tmpSpellEntity != spellEntity) && entityWorld.hasComponent(tmpSpellEntity, UniqueComponent.class)){
+                                NameComponent tmpNameComponent = entityWorld.getComponent(tmpSpellEntity, NameComponent.class);
+                                if((tmpNameComponent != null) && (tmpNameComponent.getName().equals(nameComponent.getName()))){
+                                    setOnCooldown(entityWorld, tmpSpellEntity);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private static void setOnCooldown(EntityWorld entityWorld, int spellEntity){
         CooldownComponent cooldownComponent = entityWorld.getComponent(spellEntity, CooldownComponent.class);
         if(cooldownComponent != null){
             entityWorld.setComponent(spellEntity, new RemainingCooldownComponent(cooldownComponent.getDuration()));
