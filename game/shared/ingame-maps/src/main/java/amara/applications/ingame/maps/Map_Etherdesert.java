0a7bc1c0590b91139853e383f93a48e8485eac6c
destroflyer <destro-flyer@web.de>
2019-06-02 14:52:10 +0200

    Added more towers and waves and balancing to Etherdesert.

--- game/shared/ingame-maps/src/main/java/amara/applications/ingame/maps/Map_Etherdesert.java
+++ game/shared/ingame-maps/src/main/java/amara/applications/ingame/maps/Map_Etherdesert.java
@@ -65,9 +65,11 @@ public class Map_Etherdesert extends Map {
         shop.setComponent(new ShopItemsComponent(
             "items/etherdesert_income",
             "items/etherdesert_tower_0",
+            "items/etherdesert_tower_7",
             "items/etherdesert_tower_5",
             "items/etherdesert_tower_1",
             "items/etherdesert_tower_6",
+            "items/etherdesert_tower_8",
             "items/etherdesert_tower_2",
             "items/etherdesert_tower_3",
             "items/etherdesert_tower_4"
@@ -203,8 +205,8 @@ public class Map_Etherdesert extends Map {
         entityWorld.setComponent(spawnCasterEntity, new TeamComponent(1));
         int spawnSourceEntity = entityWorld.createEntity();
         entityWorld.setComponent(spawnSourceEntity, new EffectCastSourceComponent(spawnCasterEntity));
-        int waves = 12;
-        int[] creepsPerWave = new int[]{ 6, 6, 4, 6, 6, 7, 28, 1, 6, 6, 2, 1 };
+        int waves = 18;
+        int[] creepsPerWave = new int[]{ 6, 6, 4, 6, 6, 7, 28, 1, 6, 6, 2, 1, 6, 6, 3, 6, 3, 1 };
         int[][] spawnTriggers = new int[waves][];
         for (int i = 0; i < waves; i++) {
             int[] waveSpawnTriggers = new int[creepsPerWave[i]];
