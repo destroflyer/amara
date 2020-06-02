2fd4c6efd8c9572144c35269c168bc17f4ed6193
carlkuesters <carl.kuesters@youengineering.com>
2020-06-03 01:19:01 +0200

    Make TestGame more dynamic in regards of authToken via runconfig.

--- game/test/src/main/java/amara/test/TestGame.java
+++ game/test/src/main/java/amara/test/TestGame.java
@@ -18,20 +18,21 @@ import amara.libraries.network.exceptions.*;
  *
  * @author Carl
  */
-public class TestGame{
+public class TestGame {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        String authToken = args[0];
         Launcher_Core.initialize();
         Launcher_Game.initialize();
-        //Server
+        // Server
         MasterserverServerApplication masterServer = new MasterserverServerApplication(33900);
         masterServer.start();
-        //Client
-        try{
+        // Client
+        try {
             MasterserverClientApplication masterClient = new MasterserverClientApplication(new HostInformation("localhost", masterServer.getPort()));
             masterClient.start();
             NetworkClient networkClient = masterClient.getStateManager().getState(NetworkClientHeadlessAppState.class).getNetworkClient();
-            networkClient.sendMessage(new Message_Login("myAuthToken"));
+            networkClient.sendMessage(new Message_Login(authToken));
             networkClient.sendMessage(new Message_CreateLobby());
             networkClient.sendMessage(new Message_SetLobbyData(new LobbyData("arama", new TeamFormat(2, 0))));
             networkClient.sendMessage(new Message_AddLobbyBot(new LobbyPlayer_Bot(BotType.EASY, "Bot", new GameSelectionPlayerData(11, null))));
@@ -39,7 +40,7 @@ public class TestGame{
             networkClient.sendMessage(new Message_AcceptGameSelection(true));
             networkClient.sendMessage(new Message_SetGameSelectionPlayerData(new GameSelectionPlayerData(11, null)));
             networkClient.sendMessage(new Message_LockInGameSelection());
-        }catch(ServerConnectionException | ServerConnectionTimeoutException ex){
+        } catch(ServerConnectionException | ServerConnectionTimeoutException ex) {
             ex.printStackTrace();
         }
     }
