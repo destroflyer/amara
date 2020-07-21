5fbcd56ab8d406ef3fa572645338bcba8e4e16b0
carlkuesters <carl.kuesters@youengineering.com>
2020-07-21 13:32:07 +0200

    Added pathfinding to dwarf warrior ult (charge).

--- game/shared/ingame-entitysystem/src/main/java/amara/applications/ingame/entitysystem/components/movements/MovementPathfindingComponent.java
+++ game/shared/ingame-entitysystem/src/main/java/amara/applications/ingame/entitysystem/components/movements/MovementPathfindingComponent.java
@@ -1,19 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package amara.applications.ingame.entitysystem.components.movements;
 
 import com.jme3.network.serializing.Serializable;
 
-/**
- *
- * @author Carl
- */
 @Serializable
-public class MovementPathfindingComponent{
+public class MovementPathfindingComponent {
 
-    public MovementPathfindingComponent(){
-        
+    public MovementPathfindingComponent() {
+
     }
 }
