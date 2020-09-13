56614cf1cae18fda082a5632a6e9640eb1f39c20
carlkuesters <carl.kuesters@youengineering.com>
2020-09-13 23:58:59 +0200

    Polished Alice character skills.

--- shared/ingame-entitysystem/src/main/java/amara/applications/ingame/entitysystem/systems/units/PassiveUtil.java
+++ shared/ingame-entitysystem/src/main/java/amara/applications/ingame/entitysystem/systems/units/PassiveUtil.java
@@ -1,89 +1,90 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package amara.applications.ingame.entitysystem.systems.units;
 
 import java.util.LinkedList;
-import amara.applications.ingame.entitysystem.components.general.*;
-import amara.applications.ingame.entitysystem.components.units.*;
-import amara.applications.ingame.entitysystem.components.units.passives.*;
+
+import amara.applications.ingame.entitysystem.components.general.NameComponent;
+import amara.applications.ingame.entitysystem.components.general.UniqueComponent;
+import amara.applications.ingame.entitysystem.components.units.CurrentPassivesComponent;
+import amara.applications.ingame.entitysystem.components.units.passives.PassiveAddedEffectTriggersComponent;
+import amara.applications.ingame.entitysystem.components.units.passives.PassiveRemovedEffectTriggersComponent;
 import amara.applications.ingame.entitysystem.systems.effects.triggers.EffectTriggerUtil;
 import amara.core.Util;
 import amara.libraries.entitysystem.EntityWorld;
 
-/**
- *
- * @author Carl
- */
-public class PassiveUtil{
-    
-    private static LinkedList<Integer> tmpPassiveEntities_Current = new LinkedList<Integer>();
-    private static LinkedList<Integer> tmpPassiveEntities_Changed = new LinkedList<Integer>();
-    
-    public static void addPassives(EntityWorld entityWorld, int targetEntity, int... passiveEntities){
+public class PassiveUtil {
+
+    private static LinkedList<Integer> tmpPassiveEntities_Current = new LinkedList<>();
+    private static LinkedList<Integer> tmpPassiveEntities_Changed = new LinkedList<>();
+
+    public static void addPassives(EntityWorld entityWorld, int targetEntity, int... passiveEntities) {
         triggerPassives(entityWorld, targetEntity, passiveEntities, true);
     }
-    
-    public static void removePassives(EntityWorld entityWorld, int targetEntity, int... passiveEntities){
+
+    public static void removePassives(EntityWorld entityWorld, int targetEntity, int... passiveEntities) {
         triggerPassives(entityWorld, targetEntity, passiveEntities, false);
     }
-    
-    private static void triggerPassives(EntityWorld entityWorld, int targetEntity, int[] passiveEntities, boolean addedOrRemoved){
+
+    private static void triggerPassives(EntityWorld entityWorld, int targetEntity, int[] passiveEntities, boolean addedOrRemoved) {
         tmpPassiveEntities_Current.clear();
         tmpPassiveEntities_Changed.clear();
         CurrentPassivesComponent currentPassivesComponent = entityWorld.getComponent(targetEntity, CurrentPassivesComponent.class);
-        if(currentPassivesComponent != null){
-            for(int passiveEntity : currentPassivesComponent.getPassiveEntities()){
+        if (currentPassivesComponent != null) {
+            for (int passiveEntity : currentPassivesComponent.getPassiveEntities()) {
                 tmpPassiveEntities_Current.add(passiveEntity);
             }
         }
-        for(int passiveEntity : passiveEntities){
+        for (int passiveEntity : passiveEntities) {
             boolean hasChanged = true;
             int uniquePassiveEntity = passiveEntity;
-            if(entityWorld.hasComponent(passiveEntity, UniqueComponent.class)){
+            if (entityWorld.hasComponent(passiveEntity, UniqueComponent.class)) {
                 NameComponent nameComponent = entityWorld.getComponent(passiveEntity, NameComponent.class);
-                if(nameComponent != null){
+                if (nameComponent != null) {
                     int uniquePassivesCount = 0;
-                    for(int i=(tmpPassiveEntities_Current.size() - 1);i>=0;i--){
+                    for (int i = (tmpPassiveEntities_Current.size() - 1); i >= 0; i--) {
                         int tmpPassiveEntity = tmpPassiveEntities_Current.get(i);
-                        if(entityWorld.hasComponent(tmpPassiveEntity, UniqueComponent.class)){
+                        if (entityWorld.hasComponent(tmpPassiveEntity, UniqueComponent.class)) {
                             NameComponent tmpNameComponent = entityWorld.getComponent(tmpPassiveEntity, NameComponent.class);
-                            if((tmpNameComponent != null) && nameComponent.getName().equals(tmpNameComponent.getName())){
-                                if(uniquePassivesCount == 0){
+                            if ((tmpNameComponent != null) && nameComponent.getName().equals(tmpNameComponent.getName())) {
+                                if (uniquePassivesCount == 0) {
                                     uniquePassiveEntity = tmpPassiveEntity;
                                 }
                                 uniquePassivesCount++;
                             }
                         }
                     }
-                    if((addedOrRemoved && (uniquePassivesCount > 0))
-                    || ((!addedOrRemoved) && (uniquePassivesCount != 1))){
+                    if ((addedOrRemoved && (uniquePassivesCount > 0))
+                    || ((!addedOrRemoved) && (uniquePassivesCount != 1))) {
                         hasChanged = false;
                     }
                 }
             }
-            if(addedOrRemoved){
+            if (addedOrRemoved) {
                 tmpPassiveEntities_Current.add(passiveEntity);
-            }
-            else{
+            } else {
                 tmpPassiveEntities_Current.remove((Integer) uniquePassiveEntity);
             }
-            if(hasChanged){
+            if (hasChanged) {
                 tmpPassiveEntities_Changed.add(uniquePassiveEntity);
             }
         }
         entityWorld.setComponent(targetEntity, new CurrentPassivesComponent(Util.convertToArray_Integer(tmpPassiveEntities_Current)));
-        for(int passiveEntity : tmpPassiveEntities_Changed){
-            int[] effectTriggersEntities;
-            if(addedOrRemoved){
-                effectTriggersEntities = entityWorld.getComponent(passiveEntity, PassiveAddedEffectTriggersComponent.class).getEffectTriggerEntities();
+        for (int passiveEntity : tmpPassiveEntities_Changed) {
+            int[] effectTriggersEntities = null;
+            if (addedOrRemoved) {
+                PassiveAddedEffectTriggersComponent passiveAddedEffectTriggersComponent = entityWorld.getComponent(passiveEntity, PassiveAddedEffectTriggersComponent.class);
+                if (passiveAddedEffectTriggersComponent != null) {
+                    effectTriggersEntities = passiveAddedEffectTriggersComponent.getEffectTriggerEntities();
+                }
+            } else {
+                PassiveRemovedEffectTriggersComponent passiveRemovedEffectTriggersComponent = entityWorld.getComponent(passiveEntity, PassiveRemovedEffectTriggersComponent.class);
+                if (passiveRemovedEffectTriggersComponent != null) {
+                    effectTriggersEntities = passiveRemovedEffectTriggersComponent.getEffectTriggerEntities();
+                }
             }
-            else{
-                effectTriggersEntities = entityWorld.getComponent(passiveEntity, PassiveRemovedEffectTriggersComponent.class).getEffectTriggerEntities();
-            }
-            for(int effectTriggerEntity : effectTriggersEntities){
-                EffectTriggerUtil.triggerEffect(entityWorld, effectTriggerEntity, -1);
+            if (effectTriggersEntities != null) {
+                for (int effectTriggerEntity : effectTriggersEntities) {
+                    EffectTriggerUtil.triggerEffect(entityWorld, effectTriggerEntity, -1);
+                }
             }
         }
     }
