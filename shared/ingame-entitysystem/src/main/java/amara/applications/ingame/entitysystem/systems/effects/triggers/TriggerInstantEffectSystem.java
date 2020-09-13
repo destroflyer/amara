d27c93d9d1a5be4a5ac6c3e17a93481f2004556a
carlkuesters <carl.kuesters@youengineering.com>
2020-09-13 17:25:23 +0200

    Polished Garmon character and fixed a few typos in formulas.

--- shared/ingame-entitysystem/src/main/java/amara/applications/ingame/entitysystem/systems/effects/triggers/TriggerInstantEffectSystem.java
+++ shared/ingame-entitysystem/src/main/java/amara/applications/ingame/entitysystem/systems/effects/triggers/TriggerInstantEffectSystem.java
@@ -1,15 +1,19 @@
 package amara.applications.ingame.entitysystem.systems.effects.triggers;
 
-import amara.applications.ingame.entitysystem.components.units.effecttriggers.*;
-import amara.applications.ingame.entitysystem.components.units.effecttriggers.triggers.*;
-import amara.libraries.entitysystem.*;
+import amara.applications.ingame.entitysystem.components.spells.RemainingCooldownComponent;
+import amara.applications.ingame.entitysystem.components.units.effecttriggers.TriggerSourceComponent;
+import amara.applications.ingame.entitysystem.components.units.effecttriggers.triggers.InstantTriggerComponent;
+import amara.libraries.entitysystem.EntitySystem;
+import amara.libraries.entitysystem.EntityWorld;
 
 public class TriggerInstantEffectSystem implements EntitySystem {
 
     @Override
     public void update(EntityWorld entityWorld, float deltaSeconds) {
         for (int effectTriggerEntity : entityWorld.getEntitiesWithAll(InstantTriggerComponent.class, TriggerSourceComponent.class)) {
-            EffectTriggerUtil.triggerEffect(entityWorld, effectTriggerEntity, -1);
+            if (!entityWorld.hasComponent(effectTriggerEntity, RemainingCooldownComponent.class)) {
+                EffectTriggerUtil.triggerEffect(entityWorld, effectTriggerEntity, -1);
+            }
         }
     }
 }
